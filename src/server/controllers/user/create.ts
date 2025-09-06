72761b326af2756ca615ec096e8dd8c85a82c110
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-09-05 22:11:25 -0300

    chore: add yup for input validation

--- src/server/controllers/user/create.ts
+++ src/server/controllers/user/create.ts
@@ -1,18 +1,47 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
+import * as yup from 'yup'
 
 type UserTypes = {
   name: string
+  email: string
+  password: string
 }
 
-function create(
+async function create(
   request: Request<{}, {}, UserTypes>,
   response: Response
 ){
-
-  return response.status(StatusCodes.CREATED).json({
-    message: 'Create user'
+  const bodySchema: yup.Schema<UserTypes> = yup.object().shape({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().min(6).required()
   })
+
+  try {
+    const { name, email, password } = await bodySchema.validate(request.body, { abortEarly: false })
+
+    return response.status(StatusCodes.CREATED).json({
+      status: true,
+      data: { name, email, password },
+      message: 'Ok'
+    })
+
+  } catch (error) {
+    if (error instanceof yup.ValidationError) {
+      return response.status(StatusCodes.BAD_REQUEST).json({
+        status: false,
+        data: null,
+        message: error.errors
+      })
+    }
+
+    return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: false,
+      data: null,
+      message: 'Internal server error'
+    })
+  }
 }
 
 export { create }
