e2ee6d0fb5e64569ccf364cc21ed965e33abbef5
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-09-24 20:59:41 -0300

    feat: add database connection check before starting the server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-import 'dotenv/config'
+import { connection } from './database/knex/connection'
+import { env } from './env'
 
 import { server } from './server/server'
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log('~ server is running')
-})
+const startServer = () => {
+  server.listen(env.PORT, () => {
+    console.log('~ server is running')
+  })
+}
+
+const bootstrap = async () => {
+  try {
+    await connection.raw('select 1+1 as result')
+    console.log('~ database connection successful')
+
+    startServer()
+  } catch (err) {
+    console.error('x failed to start the application:', err instanceof Error ? err.message : err)
+    process.exit(1)
+  }
+}
+
+bootstrap()
