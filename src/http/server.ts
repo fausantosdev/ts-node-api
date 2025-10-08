da51c6a51de512aa32acbfe533073277e49efd2e
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-10-08 19:22:59 -0300

    feat: add global error handler to handle application exceptions

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,27 @@
-import express from 'express'
-import './../shared/services/yup-translations'
+import express, { Request, Response, NextFunction } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
+import './../shared/services/yup-translations'
 import { routes } from './routes'
+import { responseHelper } from '../shared/helpers/response-helper'
 
 const server = express()
 
 server.use(express.json())
 
 routes(server)
+
+server.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err) {
+    return res
+      .status(err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(responseHelper({
+        status: false,
+        errors: err.message
+      }))
+  }
+
+  next()
+})
 
 export { server }
