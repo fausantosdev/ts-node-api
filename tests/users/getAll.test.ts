773fc759f455226a4db1b103b9519096898fa064
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-10-09 23:57:44 -0300

    test: add integration test setup with database and authentication

--- tests/users/getAll.test.ts
+++ tests/users/getAll.test.ts
@@ -1,10 +1,44 @@
 import { StatusCodes } from 'http-status-codes'
 import { testServer } from '../jest.setup'
+import { connection } from '../../src/database/knex/connection'
+
+const userMock = {
+  name: 'John Doe',
+  email: 'johndoe@gmail.com',
+  password: 'abcd1234'
+}
+
+let accessToken: ''
+
+beforeAll(async () =>{
+  await connection.migrate.up()
+
+  await testServer
+    .post('/user')
+    .send(userMock)
+
+  const signInResponse = await testServer
+    .post('/auth/sign-in')
+    .send({
+      email: userMock.email,
+      password: userMock.password
+    })
+
+  accessToken = signInResponse.body.data
+})
+
+afterAll(async () =>{
+  await connection.migrate.down()
+  await connection.destroy()
+})
 
 describe('Users - getAll', () => {
   it('Should return 400 if the query\'s \'page\' parameter is different from an integer', async () => {
     const response = await testServer
       .get('/user?page=b')
+      .auth(accessToken, { type: 'bearer' })
+
+    console.log(response.body)
 
     expect(response.statusCode).toBe(StatusCodes.BAD_REQUEST)
     expect(response.body.errors.query.page).toEqual('Formato digitado é invalido')
