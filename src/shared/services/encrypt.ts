909548124cd55cac0a253dc59420c094063a8260
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2026-03-04 20:53:18 -0300

    feat: add APP_KEY as pepper in bcrypt hashing and increase salt rounds

--- src/shared/services/encrypt.ts
+++ src/shared/services/encrypt.ts
@@ -1,12 +1,13 @@
 import bcrypt from 'bcrypt'
+import { env } from '../../env'
 
 const encrypt = {
   async hash(value: string): Promise<string> {
-    return await bcrypt.hash(value, 10)
+    return await bcrypt.hash(env.APP_KEY + value, 13)
   },
 
   async compare(value: string, hashed: string): Promise<boolean> {
-    return await bcrypt.compare(value, hashed)
+    return await bcrypt.compare(env.APP_KEY + value, hashed)
   }
 }
 
