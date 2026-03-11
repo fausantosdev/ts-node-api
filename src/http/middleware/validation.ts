33b3a0404d84698796e4380fa1a23bd76dcec442
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2026-03-11 20:20:20 -0300

    refactor: simplify validation error handling

--- src/http/middleware/validation.ts
+++ src/http/middleware/validation.ts
@@ -7,33 +7,28 @@ type PropertyTypes = 'body' | 'header' | 'params' | 'query'
 type GetSchemaTypes = <T>(schema: Schema<T>) => Schema<T>
 type AllSchemaTypes = Record<PropertyTypes, Schema<any>>
 type GetAllSchemasTypes = (getSchema: GetSchemaTypes) => Partial<AllSchemaTypes>
-                              // ↓↓ Partial pega um tipo T qualquer e transforma todas as propriedades obrigatórias em opcionais.
+
 type ValidationTypes = (getAllSchemas: GetAllSchemasTypes) => RequestHandler
 
-// Função que retorna um middleware de validação genérico
 const validation: ValidationTypes = (getAllSchemas) =>
   async (request, response, next) => {
     const schemas = getAllSchemas(schema => schema)
 
-    const errorsResult: Record<string, Record<string, string>> = {}
+    const errorsResult: string[] = []
 
     Object.entries(schemas).forEach(([key, schema]) => {
       try {
         schema.validateSync(request[key as PropertyTypes], { abortEarly: false })
-        //        ↑↑↑ Ao contrário do validate, o validateSync espera a validação ser concluída antes de continuar a execução do código
       } catch (error) {
         const yupError = error as ValidationError
-        const errors: Record<string, string> = {}
 
         yupError.inner.forEach(error => {
-          if (error.path !== undefined) errors[error.path] = error.message
+          if (error.path !== undefined) errorsResult.push(error.message)
         })
-
-        errorsResult[key] = errors
       }
     })
 
-    if (Object.entries(errorsResult).length === 0) {
+    if (errorsResult.length === 0) {
       return next()
     } else {
       return response
