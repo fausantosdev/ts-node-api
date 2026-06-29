112107fb324d0b871ae2a41c35323aff2230f080
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2026-06-28 21:15:58 -0300

    fix: enable cascade actions on point_items foreign keys

--- src/database/knex/migrations/20251020003405_create_point_items.ts
+++ src/database/knex/migrations/20251020003405_create_point_items.ts
@@ -7,8 +7,20 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema
       .createTable(table, function (table) {
         table.increments('id').primary().index()
-        table.integer('point_id').notNullable().references('id').inTable('points')
-        table.integer('item_id').notNullable().references('id').inTable('items')
+        table
+          .integer('point_id')
+          .notNullable()
+          .references('id')
+          .inTable('points')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE')
+        table
+          .integer('item_id')
+          .notNullable()
+          .references('id')
+          .inTable('items')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE')
       })
         .then(()=> console.log(`[✓] table ${table} created`))
         .catch(error => console.log(error.message))
